3acc63bac6e80214e9665413ff632e6768526d3e
Kyle Gorman <kylebgorman@gmail.com>
2014-09-18 22:27:38 -0700

    cleanup

--- untokenize.pl
+++ untokenize.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl
 # untokenize.pl: undo Penn Treebank tokenization
 # Kyle Gorman <gormanky@ohsu.edu>
-# 
-# no copyright claimed: this is just too simplistic
-
 
 use strict;
 use warnings;
@@ -12,21 +9,29 @@ my $replace = "";                   # replacement string, usually null
 my $holder = "<rAnD0mPlaceHolder>"; # placeholder string, to be removed
 
 while (<>) {
+    # easy punctuation
     s/ ([:;,\.\?!%]|'')/$holder$1/g;
+    # clitics
     s/ ('m|'d|'s|n't|'ll|'ve|'re)/$holder$1/gi;
+    # left quotes
     s/(``) /$1$holder/g;
-    s/(?<=\s)(\$) /$1$holder/g;  # don't in cases like "US$ "
+    # difficult punctuation
+    s/(?<=\s)(\$) /$1$holder/g;
     s/ (' )/$holder$1/g;
     s/ (--) /$holder$1$holder/g;
+    # brackets
     s/(\(|-LRB-) /\($holder/g;
     s/(\[|-LSB-) /\[$holder/g;
     s/(\{|-LCB-) /\{$holder/g;
     s/ (\)|-RRB-)/\)$holder/g;
     s/ (\]|-RSB-)/\]$holder/g;
     s/ (\}|-RCB-)/\}$holder/g;
+    # fraction
     s/\\\//\//g;
-    s/(?<=\.$holder)\.//g;       # fix occasional "U.S.." examples
+    # fix final \.\. (occasional "U.S.."
+    s/(?<=\.$holder)\.//g;
     s/$holder(\.\.\.)/ $1/g;
+    # replace placeholder string with replacement string
     s/$holder/$replace/g;
     print;
 }
