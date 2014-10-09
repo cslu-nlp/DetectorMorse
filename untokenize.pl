b512df452fa15d03ce3b0c6754ef4975e9163c43
Kyle Gorman <gormanky@ohsu.edu>
2014-10-08 19:10:21 -0700

    improvements to untokenization

--- untokenize.pl
+++ untokenize.pl
@@ -14,10 +14,10 @@ while (<>) {
     # clitics
     s/ ('m|'d|'s|n't|'ll|'ve|'re)/$holder$1/gi;
     # left quotes
-    s/(``) /$1$holder/g;
+    s/(`+) /$1$holder/g;
     # difficult punctuation
     s/(?<=\s)(\$) /$1$holder/g;
-    s/ (' )/$holder$1/g;
+    s/ ('+($holder| ))/$holder$1/g;
     s/ (--) /$holder$1$holder/g;
     # brackets
     s/(\(|-LRB-) /\($holder/g;
