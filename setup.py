4953d86ea60cc7ca0bc3a8c322ce5f1904aba198
Kyle Gorman <kylebgorman@gmail.com>
2017-11-02 15:13:40 -0400

    More modernization/cleanup.

--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ setup(name="DetectorMorse",
       version="0.2",
       description="DetectorMorse, a sentence splitter",
       author="Kyle Gorman",
-      author_email="gormanky@ohsu.edu",
-      packages=["detectormorse"],
-      install_requires=["nlup >= 0.3.0"],
-      dependency_links=["http://github.com/cslu-nlp/nlup/archive/master.zip#egg=nlup-0.3.0"])
+      author_email="kylebgorman@gmail.com",
+      packages=["detectormorse"])
