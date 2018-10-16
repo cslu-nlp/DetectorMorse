b983b7819b545e991ac5b3c39a975577fe528e7b
Kyle Gorman <kylebgorman@gmail.com>
2018-10-16 14:06:59 -0400

    Adds long description to setup.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
+from os import path
 from setuptools import setup
+
+description = ("Core libraries for natural language processing",)
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(name="DetectorMorse",
       version="0.4.0",
       description="DetectorMorse, a sentence splitter",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author="Kyle Gorman",
       author_email="kylebgorman@gmail.com",
       packages=["detectormorse"],
